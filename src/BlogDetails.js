8096bd9d876857134b3a33fcb738feee50a83f40
brlojam4932 <36285465+brlojam4932@users.noreply.github.com>
2021-10-08 16:12:12 -0700

    delete
    added delete functions and buttons

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,21 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import useFetch from "./useFetch";
 
 // use of custom hook here, to fetch the url
 const BlogDetails = () => {
   const { id } = useParams();
   const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+  const history = useHistory();
+
+  // we create a function to associate the onClick delete button
+  const handleDelete = () => {
+    fetch('http://localhost:8000/blogs/' + blog.id, { // I think we can use either id or blog.id
+      method: "DELETE"
+    }).then(() => {
+      history.push('/'); // push takes us back to home page
+
+    })
+  }
 
   // the && operator only evaluates the right side if the left side is true
   return (
@@ -16,6 +27,7 @@ const BlogDetails = () => {
           <h2>{ blog.title}</h2>
           <p>Written by { blog.author } </p>
           <div>{ blog.body }</div>
+          <button onClick={handleDelete} >delete</button>
 
         </article>
       ) }
